78949e910292efee19909701e22da513d46dd424
xloc <xloc.cc@outlook.com>
2024-01-16 01:49:15 -0800

    usable editor done

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,20 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import * as monaco from "monaco-editor";
+
+const editor = ref();
+onMounted(() => {
+  monaco.editor.create(editor.value, {
+    value: 'function x() {\n\tconsole.log("Hello world!");\n}',
+    language: "javascript",
+    automaticLayout: true,
+  });
+});
 </script>
 
-<template></template>
+<template>
+  <div class="h-screen flex items-stretch w-full">
+    <div class="w-14 bg-stone-300"></div>
+    <div ref="editor" class="flex-1"></div>
+  </div>
+</template>
